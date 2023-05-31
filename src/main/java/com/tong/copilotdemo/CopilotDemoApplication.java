b6f250dc15460c988f7570c7c5cb10986a97b710
lanqitong <lanqt@chinatelecom.cn>
2023-05-31 09:50:45 +0800

    init

--- src/main/java/com/tong/copilotdemo/CopilotDemoApplication.java
+++ src/main/java/com/tong/copilotdemo/CopilotDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CopilotDemApplication {
+public class CopilotDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CopilotDemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CopilotDemoApplication.class, args);
+    }
 
 }
