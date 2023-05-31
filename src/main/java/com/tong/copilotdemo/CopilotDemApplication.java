021a8476abef2e195e765e8b15cf4c21d6efceb2
lanqitong <lanqt@chinatelecom.cn>
2023-05-31 09:43:08 +0800

    Initial commit

---
+++ src/main/java/com/tong/copilotdemo/CopilotDemApplication.java
@@ -0,0 +1,13 @@
+package com.tong.copilotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotDemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotDemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tong/copilotdemo/CopilotDemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.copilotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotDemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
